718b0bc02d221ab0870e22b42d72d2a06c503681
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-22 17:40:40 +0530

    Add project files.

---
+++ Constructor chenging with one class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Constructor_chenging_with_one_class
+{
+    class Program
+    {
+        // this is a constructor overloding  
+        Program() : this(20)
+        {
+            Console.WriteLine("Hello :");
+        }
+        Program(int x)
+        {
+            Console.WriteLine("Hiiiiii");
+        }
+        Program(string x):this()
+        {
+            Console.WriteLine("Bye ");
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program("A");
+            
+        }
+    }
+}
